99abb75f64b0295a3553559accfb162c23e532aa
axect <edeftg@gmail.com>
2018-11-03 02:46:45 +0900

    Rand

--- src/matlab_macro.rs
+++ src/matlab_macro.rs
@@ -22,3 +22,31 @@ macro_rules! zeros {
         vec![0f64; $n]
     };
 }
+
+/// MATLAB like rand
+///
+/// # Examples
+/// ```
+/// extern crate peroxide;
+/// use peroxide::*;
+/// 
+/// let a = rand!(2, 2);
+/// println!("{}", a); // 2 x 2 random matrix (0 ~ 1)
+/// ```
+#[macro_export]
+macro_rules! rand {
+    ( $m:expr, $n:expr ) => {
+        {   
+            let r = $m;
+            let c = $n;
+            let mut rng = thread_rng();
+            let mut m = matrix(vec![0f64; r * c], r, c, Row);
+            for i in 0 .. r {
+                for j in 0 .. c {
+                    m[(i, j)] = rng.gen_range(0f64, 1f64);
+                }
+            }
+            m
+        }
+    };
+}
