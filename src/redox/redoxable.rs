3e24660578dcd9c966a0467901b2fc0c868ffda8
Axect <axect@outlook.kr>
2020-03-03 17:32:57 +0900

    Make Redox useful
    Implement `std::ops` for `RedoxVector`

--- src/redox/redoxable.rs
+++ src/redox/redoxable.rs
@@ -1,5 +1,7 @@
-use std::ops::{Add, Deref, Index, IndexMut};
+use std::ops::{Add, Deref, Index, IndexMut, Sub, Neg, Mul, Div};
 use structure::vector::VecOps;
+use std::net::Shutdown::Read;
+use PowOps;
 
 /// Smart pointer of Vector
 pub struct RedoxVector {
@@ -42,6 +44,70 @@ impl IndexMut<usize> for RedoxVector {
     }
 }
 
+impl Neg for RedoxVector {
+    type Output = RedoxVector;
+
+    fn neg(self) -> Self::Output {
+        RedoxVector::from_vec(self.data.into_iter().map(|x| -x).collect())
+    }
+}
+
+impl Add<f64> for RedoxVector {
+    type Output = RedoxVector;
+
+    fn add(self, rhs: f64) -> Self::Output {
+        RedoxVector::from_vec(self.data.s_add(rhs))
+    }
+}
+
+impl Sub<f64> for RedoxVector {
+    type Output = RedoxVector;
+
+    fn sub(self, rhs: f64) -> Self::Output {
+        RedoxVector::from_vec(self.data.s_sub(rhs))
+    }
+}
+
+impl Mul<f64> for RedoxVector {
+    type Output = RedoxVector;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        RedoxVector::from_vec(self.data.s_mul(rhs))
+    }
+}
+
+impl Div<f64> for RedoxVector {
+    type Output = RedoxVector;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        RedoxVector::from_vec(self.data.s_div(rhs))
+    }
+}
+
+impl Add<RedoxVector> for f64 {
+    type Output = RedoxVector;
+
+    fn add(self, rhs: RedoxVector) -> Self::Output {
+        rhs.add(self)
+    }
+}
+
+impl Sub<RedoxVector> for f64 {
+    type Output = RedoxVector;
+
+    fn sub(self, rhs: RedoxVector) -> Self::Output {
+        RedoxVector::from_vec(rhs.data.into_iter().map(|x| self - x).collect())
+    }
+}
+
+impl Mul<RedoxVector> for f64 {
+    type Output = RedoxVector;
+
+    fn mul(self, rhs: RedoxVector) -> Self::Output {
+        rhs.mul(self)
+    }
+}
+
 impl Add<RedoxVector> for RedoxVector {
     type Output = RedoxVector;
 
@@ -55,5 +121,49 @@ impl<'a, 'b> Add<&'b RedoxVector> for &'a RedoxVector {
 
     fn add(self, rhs: &'b RedoxVector) -> Self::Output {
         RedoxVector::from_vec(self.data.add(&rhs.data))
+    }
+}
+
+impl Sub<RedoxVector> for RedoxVector {
+    type Output = RedoxVector;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        RedoxVector::from_vec(self.data.sub(&rhs.data))
+    }
+}
+
+impl<'a, 'b> Sub<&'b RedoxVector> for &'a RedoxVector {
+    type Output = RedoxVector;
+
+    fn sub(self, rhs: &'b RedoxVector) -> Self::Output {
+        RedoxVector::from_vec(self.data.sub(&rhs.data))
+    }
+}
+
+/// Dot product
+impl Mul<RedoxVector> for RedoxVector {
+    type Output = f64;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.data.dot(&rhs.data)
+    }
+}
+
+impl<'a, 'b> Mul<&'b RedoxVector> for &'a RedoxVector {
+    type Output = f64;
+
+    fn mul(self, rhs: &'b RedoxVector) -> Self::Output {
+        self.data.dot(&rhs.data)
+    }
+}
+
+/// Redox wrap for Vector
+pub trait Redoxable {
+    fn redox(self) -> RedoxVector;
+}
+
+impl Redoxable for Vec<f64> {
+    fn redox(self) -> RedoxVector {
+        RedoxVector::from_vec(self)
     }
 }
