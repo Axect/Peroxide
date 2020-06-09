d53a69420decb68f627b3ac06e427fa30c4dd1b6
Axect <axect@outlook.kr>
2020-06-09 14:54:33 +0900

    Enhance Test
    * Add Test Data File
    * Inverse test complete

---
+++ tests/inv.rs
@@ -0,0 +1,14 @@
+extern crate peroxide;
+use peroxide::fuga::*;
+
+#[cfg(feature = "dataframe")]
+#[test]
+fn test_inverse() {
+    for i in 6 .. 11 {
+        let df = DataFrame::read_nc_by_header(&format!("test_data/inv/inverse_{}.nc", i), vec!["a", "b"]).unwrap();
+        let a: Matrix = matrix(df["a"].clone(), i, i, Col);
+        let b: Matrix = matrix(df["b"].clone(), i, i, Col);
+        let c = a.inv().unwrap();
+        assert_eq!(b, c);
+    }
+}
