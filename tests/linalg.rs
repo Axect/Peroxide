65455c9054d719b5b325e60d2f1e1cce5a749bdb
Axect <axect@outlook.kr>
2020-06-14 20:21:41 +0900

    Add solve test
    * WAZ works great
    * LU has a problem

--- tests/linalg.rs
+++ tests/linalg.rs
@@ -62,3 +62,21 @@ fn test_pinv() {
         assert_eq!(b, c);
     }
 }
+
+#[cfg(feature = "dataframe")]
+#[test]
+fn test_solve() {
+    for i in 5 .. 21 {
+        let df = DataFrame::read_nc_by_header(&format!("test_data/rand_mat/randmat_{}.nc", i), vec!["m", "v", "sol"]).unwrap();
+        let a: Matrix = matrix(df["m"].clone(), i, i, Col);
+        let b = df["v"].clone();
+        let x = df["sol"].clone();
+        let c = a.solve(&b, LU);
+        let d = a.solve(&b, WAZ);
+        x.print();
+        c.print();
+        d.print();
+        //assert!(eq_vec(&x, &c, 1e-6));
+        assert!(eq_vec(&x, &d, 1e-6));
+    }
+}
