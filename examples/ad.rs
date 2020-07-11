7e02f1c8de4c0161ede0299ae24d4b103d111a9e
Axect <axect@outlook.kr>
2020-07-11 17:56:15 +0900

    More example about AD

--- examples/ad.rs
+++ examples/ad.rs
@@ -4,6 +4,9 @@ use peroxide::fuga::*;
 
 fn main() {
     let a = AD::from_array([3f64, 1f64, 0f64]);
-    println!("{:?}", a);
-    println!("{:?}", &a * &a);
+    a.print();
+    a.powi(2).print();
+    a.sin().print();
+    a.cos().print();
+    a.tan().print();
 }
