a855e3f4e7aa566517af50bf929c7af1c5238b0b
Axect <axect@outlook.kr>
2020-10-12 21:01:31 +0900

    More print

--- src/util/print.rs
+++ src/util/print.rs
@@ -138,6 +138,86 @@ impl Printable for Vec<f64> {
     }
 }
 
+impl Printable for &Vec<usize> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<u32> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<u64> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<isize> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<i32> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<i64> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<char> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<&str> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+impl Printable for &Vec<String> {
+    fn print(&self) {
+        println!("{:?}", self);
+    }
+}
+
+
+impl Printable for &Vec<f64> {
+    fn print(&self) {
+        let mut result = String::new();
+        result.push_str("[");
+        for i in 0..self.len() {
+            let st1 = format!("{:.4}", self[i]);
+            let st2 = self[i].to_string();
+            let mut st = st2.clone();
+
+            if st1.len() < st2.len() {
+                st = st1;
+            }
+
+            result.push_str(&st);
+            if i == self.len() - 1 {
+                break;
+            }
+            result.push_str(", ");
+        }
+        result.push_str("]");
+
+        println!("{}", result);
+    }
+}
+
 impl Printable for Matrix {
     fn print(&self) {
         println!("{}", self);
