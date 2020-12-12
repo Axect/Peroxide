a830f3880d763e938cbfcd145b937cfa00643b95
Axect <axect@outlook.kr>
2020-12-12 22:45:29 +0900

    DataFrame Update: Wonderful macro

--- src/structure/dataframe.rs
+++ src/structure/dataframe.rs
@@ -92,149 +92,57 @@ pub struct Series {
     pub dtype: DType,
 }
 
+#[derive(Debug, Clone)]
+pub struct Scalar {
+    pub value: DTypeValue,
+    pub dtype: DType,
+}
+
 pub struct IndexSeries {
     pub name: String,
     pub ics: Vec<String>,
 }
 
 // =============================================================================
-// Implementations
+// Traits
 // =============================================================================
-
-impl DTypeValue {
-    pub fn to_array(&self) -> DTypeArray {
-        match self {
-            DTypeValue::USIZE(x) => DTypeArray::USIZE(vec![*x]),
-            DTypeValue::ISIZE(x) => DTypeArray::ISIZE(vec![*x]),
-            DTypeValue::U8(x) => DTypeArray::U8(vec![*x]),
-            DTypeValue::I8(x) => DTypeArray::I8(vec![*x]),
-            DTypeValue::U16(x) => DTypeArray::U16(vec![*x]),
-            DTypeValue::I16(x) => DTypeArray::I16(vec![*x]),
-            DTypeValue::U32(x) => DTypeArray::U32(vec![*x]),
-            DTypeValue::I32(x) => DTypeArray::I32(vec![*x]),
-            DTypeValue::F32(x) => DTypeArray::F32(vec![*x]),
-            DTypeValue::U64(x) => DTypeArray::U64(vec![*x]),
-            DTypeValue::I64(x) => DTypeArray::I64(vec![*x]),
-            DTypeValue::F64(x) => DTypeArray::F64(vec![*x]),
-            DTypeValue::Bool(x) => DTypeArray::Bool(vec![*x]),
-            DTypeValue::Char(x) => DTypeArray::Char(vec![*x]),
-            DTypeValue::Str(x) => DTypeArray::Str(vec![x.clone()]),
-        }
-    }
-
-    pub fn unwrap_usize(&self) -> usize {
-        match self {
-            DTypeValue::USIZE(x) => *x,
-            _ => panic!("Can't unwrap usize"),
-        }
-    }
-
-    pub fn unwrap_u8(&self) -> u8 {
-        match self {
-            DTypeValue::U8(x) => *x,
-            _ => panic!("Can't unwrap u8"),
-        }
-    }
-
-    pub fn unwrap_u16(&self) -> u16 {
-        match self {
-            DTypeValue::U16(x) => *x,
-            _ => panic!("Can't unwrap u16"),
-        }
-    }
-
-    pub fn unwrap_u32(&self) -> u32 {
-        match self {
-            DTypeValue::U32(x) => *x,
-            _ => panic!("Can't unwrap u32"),
-        }
-    }
-
-    pub fn unwrap_u64(&self) -> u64 {
-        match self {
-            DTypeValue::U64(x) => *x,
-            _ => panic!("Can't unwrap u64"),
-        }
-    }
-
-    pub fn unwrap_isize(&self) -> isize {
-        match self {
-            DTypeValue::ISIZE(x) => *x,
-            _ => panic!("Can't unwrap isize"),
-        }
-    }
-
-    pub fn unwrap_i8(&self) -> i8 {
-        match self {
-            DTypeValue::I8(x) => *x,
-            _ => panic!("Can't unwrap i8"),
-        }
-    }
-
-    pub fn unwrap_i16(&self) -> i16 {
-        match self {
-            DTypeValue::I16(x) => *x,
-            _ => panic!("Can't unwrap i16"),
-        }
-    }
-
-    pub fn unwrap_i32(&self) -> i32 {
-        match self {
-            DTypeValue::I32(x) => *x,
-            _ => panic!("Can't unwrap i32"),
-        }
-    }
-
-    pub fn unwrap_i64(&self) -> i64 {
-        match self {
-            DTypeValue::I64(x) => *x,
-            _ => panic!("Can't unwrap i64"),
-        }
-    }
-
-    pub fn unwrap_f32(&self) -> f32 {
-        match self {
-            DTypeValue::F32(x) => *x,
-            _ => panic!("Can't unwrap f32"),
-        }
-    }
-
-    pub fn unwrap_f64(&self) -> f64 {
-        match self {
-            DTypeValue::F64(x) => *x,
-            _ => panic!("Can't unwrap f64"),
-        }
-    }
-
-    pub fn unwrap_bool(&self) -> bool {
-        match self {
-            DTypeValue::Bool(x) => *x,
-            _ => panic!("Can't unwrap bool"),
-        }
-    }
-
-    pub fn unwrap_str(&self) -> String {
-        match self {
-            DTypeValue::Str(x) => x.clone(),
-            _ => panic!("Can't unwrap String"),
-        }
-    }
-
-    pub fn unwrap_char(&self) -> char {
-        match self {
-            DTypeValue::Char(x) => *x,
-            _ => panic!("Can't unwrap char"),
-        }
-    }
+pub trait TypedScalar<T> {
+    fn new(s: T) -> Self where Self: Sized;
+    fn unwrap(self) -> T;
 }
 
 pub trait TypedVector<T> {
     fn new(v: Vec<T>) -> Self;
     fn to_vec(self) -> Vec<T>;
-    fn at(&self, i: usize) -> T;
+    fn as_slice(&self) -> &[T];
+    fn at_raw(&self, i: usize) -> T;
 }
 
-macro_rules! impl_typed_array {
+
+// =============================================================================
+// Macros
+// =============================================================================
+macro_rules! impl_typed_scalar {
+    ($type:ty, $dtype:ident) => {
+        impl TypedScalar<$type> for Scalar {
+            fn new(s: $type) -> Self {
+                Self {
+                    value: DTypeValue::$dtype(s),
+                    dtype: DType::$dtype,
+                }
+            }
+
+            fn unwrap(self) -> $type {
+                match self.value {
+                    DTypeValue::$dtype(s) => s,
+                    _ => panic!("Can't unwrap {:?} value", $dtype),
+                }
+            }
+        }
+    }
+}
+
+macro_rules! impl_typed_vector {
     ($type:ty, $dtype:ident) => {
         impl TypedVector<$type> for Series {
             fn new(v: Vec<$type>) -> Self {
@@ -251,78 +159,117 @@ macro_rules! impl_typed_array {
                 }
             }
 
-            fn at(&self, i: usize) -> $type {
+            fn as_slice(&self) -> &[$type] {
                 match &self.values {
-                    DTypeArray::$dtype(v) => v[i].clone(),
-                    _ => panic!("Can't extract {:?} value", $dtype),
+                    DTypeArray::$dtype(v) => v,
+                    _ => panic!("Can't convert to {:?} vector", $dtype),
                 }
+            }
+
+            fn at_raw(&self, i: usize) -> $type {
+                let v: &[$type] = self.as_slice();
+                v[i].clone()
             }
         }
     }
 }
 
-impl_typed_array!(usize, USIZE);
-impl_typed_array!(u8, U8);
-impl_typed_array!(u16, U16);
-impl_typed_array!(u32, U32);
-impl_typed_array!(u64, U64);
-impl_typed_array!(isize, ISIZE);
-impl_typed_array!(i8, I8);
-impl_typed_array!(i16, I16);
-impl_typed_array!(i32, I32);
-impl_typed_array!(i64, I64);
-impl_typed_array!(f32, F32);
-impl_typed_array!(f64, F64);
-impl_typed_array!(bool, Bool);
-impl_typed_array!(char, Char);
-impl_typed_array!(String, Str);
+macro_rules! dtype_case {
+    ($type:ty, $value:expr, $wrapper: expr) => {{
+        let x: $type = $value;
+        $wrapper(x)
+    }};
+}
 
-//impl Series {
-//    pub fn new(values: DTypeArray) -> Self {
-//        let dtype = match values {
-//            DTypeArray::USIZE(_) => DType::USIZE,
-//            DTypeArray::U8(_) => DType::U8,
-//            DTypeArray::U16(_) => DType::U16,
-//            DTypeArray::U32(_) => DType::U32,
-//            DTypeArray::U64(_) => DType::U64,
-//            DTypeArray::ISIZE(_) => DType::ISIZE,
-//            DTypeArray::I8(_) => DType::I8,
-//            DTypeArray::I16(_) => DType::I16,
-//            DTypeArray::I32(_) => DType::I32,
-//            DTypeArray::I64(_) => DType::I64,
-//            DTypeArray::F32(_) => DType::F32,
-//            DTypeArray::F64(_) => DType::F64,
-//            DTypeArray::Bool(_) => DType::Bool,
-//            DTypeArray::Str(_) => DType::Str,
-//            DTypeArray::Char(_) => DType::Char,
-//        };
-//
-//        Self {
-//            values,
-//            dtype,
-//        }
-//    }
-//
-//    pub fn at(&self, i: usize) -> DTypeValue {
-//        match &self.values {
-//            DTypeArray::USIZE(v) => DTypeValue::USIZE(v[i]),
-//            DTypeArray::U8(v) => DTypeValue::U8(v[i]),
-//            DTypeArray::U16(v) => DTypeValue::U16(v[i]),
-//            DTypeArray::U32(v) => DTypeValue::U32(v[i]),
-//            DTypeArray::U64(v) => DTypeValue::U64(v[i]),
-//            DTypeArray::ISIZE(v) => DTypeValue::ISIZE(v[i]),
-//            DTypeArray::I8(v) => DTypeValue::I8(v[i]),
-//            DTypeArray::I16(v) => DTypeValue::I16(v[i]),
-//            DTypeArray::I32(v) => DTypeValue::I32(v[i]),
-//            DTypeArray::I64(v) => DTypeValue::I64(v[i]),
-//            DTypeArray::F32(v) => DTypeValue::F32(v[i]),
-//            DTypeArray::F64(v) => DTypeValue::F64(v[i]),
-//            DTypeArray::Bool(v) => DTypeValue::Bool(v[i]),
-//            DTypeArray::Str(v) => DTypeValue::Str(v[i].clone()),
-//            DTypeArray::Char(v) => DTypeValue::Char(v[i]),
-//        }
-//    }
-//}
+macro_rules! dtype_match {
+    ($dtype:expr, $value:expr, $wrapper:expr) => {{
+        match $dtype {
+            USIZE => dtype_case!(usize, $value, $wrapper),
+            U8 => dtype_case!(u8, $value, $wrapper),
+            U16 => dtype_case!(u16, $value, $wrapper),
+            U32 => dtype_case!(u32, $value, $wrapper),
+            U64 => dtype_case!(u64, $value, $wrapper),
+            ISIZE => dtype_case!(isize, $value, $wrapper),
+            I8 => dtype_case!(i8, $value, $wrapper),
+            I16 => dtype_case!(i16, $value, $wrapper),
+            I32 => dtype_case!(i32, $value, $wrapper),
+            I64 => dtype_case!(i64, $value, $wrapper),
+            F32 => dtype_case!(f32, $value, $wrapper),
+            F64 => dtype_case!(f64, $value, $wrapper),
+            Bool => dtype_case!(bool, $value, $wrapper),
+            Char => dtype_case!(char, $value, $wrapper),
+            Str => dtype_case!(String, $value, $wrapper),
+        }
+    }};
+
+    ($dtype:expr, $value:expr, $wrapper:expr; $functor:ident) => {{
+        match $dtype {
+            USIZE => dtype_case!($functor<usize>, $value, $wrapper),
+            U8 => dtype_case!($functor<u8>, $value, $wrapper),
+            U16 => dtype_case!($functor<u16>, $value, $wrapper),
+            U32 => dtype_case!($functor<u32>, $value, $wrapper),
+            U64 => dtype_case!($functor<u64>, $value, $wrapper),
+            ISIZE => dtype_case!($functor<isize>, $value, $wrapper),
+            I8 => dtype_case!($functor<i8>, $value, $wrapper),
+            I16 => dtype_case!($functor<i16>, $value, $wrapper),
+            I32 => dtype_case!($functor<i32>, $value, $wrapper),
+            I64 => dtype_case!($functor<i64>, $value, $wrapper),
+            F32 => dtype_case!($functor<f32>, $value, $wrapper),
+            F64 => dtype_case!($functor<f64>, $value, $wrapper),
+            Bool => dtype_case!($functor<bool>, $value, $wrapper),
+            Char => dtype_case!($functor<char>, $value, $wrapper),
+            Str => dtype_case!($functor<String>, $value, $wrapper),
+        }
+    }};
+}
+
+// =============================================================================
+// Implementations
+// =============================================================================
+
+impl Scalar {
+    pub fn to_series(self) -> Series {
+        dtype_match!(self.dtype, vec![self.unwrap()], Series::new; Vec)
+    }
+}
+
+impl Series {
+    pub fn at(&self, i: usize) -> Scalar {
+        dtype_match!(self.dtype, self.at_raw(i), Scalar::new)
+    }
+}
+
+impl_typed_scalar!(usize, USIZE);
+impl_typed_scalar!(u8, U8);
+impl_typed_scalar!(u16, U16);
+impl_typed_scalar!(u32, U32);
+impl_typed_scalar!(u64, U64);
+impl_typed_scalar!(isize, ISIZE);
+impl_typed_scalar!(i8, I8);
+impl_typed_scalar!(i16, I16);
+impl_typed_scalar!(i32, I32);
+impl_typed_scalar!(i64, I64);
+impl_typed_scalar!(f32, F32);
+impl_typed_scalar!(f64, F64);
+impl_typed_scalar!(bool, Bool);
+impl_typed_scalar!(char, Char);
+impl_typed_scalar!(String, Str);
+
+impl_typed_vector!(usize, USIZE);
+impl_typed_vector!(u8, U8);
+impl_typed_vector!(u16, U16);
+impl_typed_vector!(u32, U32);
+impl_typed_vector!(u64, U64);
+impl_typed_vector!(isize, ISIZE);
+impl_typed_vector!(i8, I8);
+impl_typed_vector!(i16, I16);
+impl_typed_vector!(i32, I32);
+impl_typed_vector!(i64, I64);
+impl_typed_vector!(f32, F32);
+impl_typed_vector!(f64, F64);
+impl_typed_vector!(bool, Bool);
+impl_typed_vector!(char, Char);
+impl_typed_vector!(String, Str);
 
 impl DataFrame {
     pub fn new(v: Vec<Series>) -> Self {
@@ -355,14 +302,15 @@ impl DataFrame {
         self.data.push(series);
     }
 
-    //pub fn row(&self, i: usize) -> DataFrame {
-    //    let mut df = DataFrame::new(vec![]);
-    //    for (j, series) in self.data.iter().enumerate() {
-    //        let new_series = Series::new(series.at(i).to_array());
-    //        df.push(&self.ics[j], new_series);
-    //    }
-    //    df
-    //}
+    pub fn row(&self, i: usize) -> DataFrame {
+        let mut df = DataFrame::new(vec![]);
+        for (j, series) in self.data.iter().enumerate() {
+            let s = series.at(i);
+            let new_series = s.to_series();
+            df.push(&self.ics[j], new_series);
+        }
+        df
+    }
 }
 
 impl Index<&str> for DataFrame {
