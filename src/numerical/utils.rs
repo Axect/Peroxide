99fc03117a36a467436641b18db40bf716430e68
Axect <axect@outlook.kr>
2021-01-17 02:06:41 +0900

    DOC: Fix documents of Jacobian

--- src/numerical/utils.rs
+++ src/numerical/utils.rs
@@ -9,7 +9,7 @@ use crate::util::non_macro::{cat, zeros};
 /// : Exact jacobian matrix using Automatic Differenitation
 ///
 /// # Type
-/// (Vector, F) -> Matrix where F: Fn(Vec<Number>) -> Vec<Number>
+/// (Vector, F) -> Matrix where F: Fn(&Vec<AD>) -> Vec<AD>
 ///
 /// # Examples
 /// ```
