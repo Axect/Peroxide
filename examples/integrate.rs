423497d71bd70e1c646bfaa288caf7f6755f1e24
Axect <axect@outlook.kr>
2021-01-21 12:36:52 +0900

    IMPL: Complete Gauss-Kronrod (#36)

---
+++ examples/integrate.rs
@@ -0,0 +1,21 @@
+extern crate peroxide;
+use peroxide::fuga::*;
+
+fn main() {
+    let a = integrate(|x| x.sin(), (1f64, 2f64), GaussLegendre(15));
+    let b = integrate(|x| x.sin(), (1f64, 2f64), GaussLegendre(29));
+    let c = integrate(|x| x.sin(), (1f64, 2f64), G7K15(1e-16));
+    let d = integrate(|x| x.sin(), (1f64, 2f64), G10K21(1e-16));
+    let e = integrate(|x| x.sin(), (1f64, 2f64), G15K31(1e-16));
+    let f = integrate(|x| x.sin(), (1f64, 2f64), G20K41(1e-16));
+    let g = integrate(|x| x.sin(), (1f64, 2f64), G25K51(1e-16));
+    let h = integrate(|x| x.sin(), (1f64, 2f64), G30K61(1e-16));
+    a.print();
+    b.print();
+    c.print();
+    d.print();
+    e.print();
+    f.print();
+    g.print();
+    h.print();
+}
