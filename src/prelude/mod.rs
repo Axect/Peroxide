c6adc5d5403964c84a18dd4d4231a2ca12ac2f45
Axect <axect@outlook.kr>
2022-11-04 10:15:57 +0900

    RLSE: Ver 0.32.1
    * `CompressionOptions` for `write_parquet`

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -99,6 +99,48 @@
 //!     a.solve(&b).print();    // [1, 1]
 //! }
 //! ```
+//!
+//! * DataFrame with Parquet
+//!
+//! ```
+//! extern crate peroxide;
+//! use peroxide::fuga::*;
+//!
+//! fn main() {
+//!     let x = seq(0, 1, 0.1);
+//!     let y = x.fmap(|t| t.powi(2));
+//!
+//!     let mut df = DataFrame::new(vec![]);
+//!     df.push("x", Series::new(x));
+//!     df.push("y", Series::new(y));
+//!
+//!     df.print();
+//!
+//!     # #[cfg(feature="parquet")] {
+//!     df.write_parquet("example_data/test.parquet", CompressionOptions::Uncompressed).unwrap();
+//!     # }
+//! }
+//! ```
+//!
+//! ```
+//! extern crate peroxide;
+//! use peroxide::prelude::*;
+//!
+//! fn main() {
+//!     let x = seq(0, 1, 0.1);
+//!     let y = x.fmap(|t| t.powi(2));
+//!
+//!     let mut df = DataFrame::new(vec![]);
+//!     df.push("x", Series::new(x));
+//!     df.push("y", Series::new(y));
+//!
+//!     df.print();
+//!
+//!     # #[cfg(feature="parquet")] {
+//!     df.write_parquet("example_data/test.parquet").unwrap();
+//!     # }
+//! }
+//! ```
 
 #[allow(unused_imports)]
 pub use crate::macros::{julia_macro::*, matlab_macro::*, r_macro::*};
