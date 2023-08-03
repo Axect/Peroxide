b3d4cbce18e002eabfa43df227cffd4d241f4741
Tae Geun Kim <axect@outlook.kr>
2023-08-03 09:14:03 +0900

    IMPL: Add more Vec<f64> algorithms
    * arg_min
    * max
    * min

--- src/traits/general.rs
+++ src/traits/general.rs
@@ -3,5 +3,8 @@ pub trait Algorithm {
     fn rank(&self) -> Vec<usize>;
     fn sign(&self) -> f64;
     fn arg_max(&self) -> usize;
+    fn arg_min(&self) -> usize;
+    fn max(&self) -> f64;
+    fn min(&self) -> f64;
     fn swap_with_perm(&mut self, p: &Vec<(usize, usize)>);
 }
