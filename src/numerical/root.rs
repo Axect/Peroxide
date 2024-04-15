eb7ad71fa9f1c7f2cf5115a20930f4a73c0e55c2
axect <ax2ct@outlook.com>
2024-04-15 11:04:15 +0900

    IMPL: Implement Broyden method

--- src/numerical/root.rs
+++ src/numerical/root.rs
@@ -172,6 +172,11 @@
 //! This leads to the `NewtonMethod` returning a `RootError::ZeroDerivative` error, which is handled in the example.
 use anyhow::{Result, bail};
 
+use crate::traits::math::{Normed, Norm, LinearOp};
+use crate::traits::sugar::{ConvToMat, VecOps};
+use crate::util::non_macro::zeros;
+use crate::structure::matrix::LinearAlgebra;
+
 /// Point alias (`[f64; N]`)
 pub type Pt<const N: usize> = [f64; N];
 /// Interval alias (`([f64; N], [f64; N])`)
@@ -242,7 +247,7 @@ pub enum RootError<const I: usize> {
     ZeroSecant(Pt<I>, Pt<I>),
 }
 
-impl std::fmt::Display for RootError<1> {
+impl<const I: usize> std::fmt::Display for RootError<I> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             RootError::NoRoot => write!(f, "There is no root in the interval"),
@@ -345,7 +350,7 @@ impl RootFinder<1, 1, (f64, f64)> for BisectionMethod {
         } else if fb.abs() < self.tol {
             return Ok([b]);
         } else if fa * fb > 0.0 {
-            bail!(RootError::NoRoot);
+            bail!(RootError::<1>::NoRoot);
         }
 
         for _ in 0..self.max_iter {
@@ -361,7 +366,7 @@ impl RootFinder<1, 1, (f64, f64)> for BisectionMethod {
                 a = c;
                 fa = fc;
             } else {
-                bail!(RootError::NoRoot);
+                bail!(RootError::<1>::NoRoot);
             }
         }
         let c = (a + b) / 2.0;
@@ -531,7 +536,7 @@ impl RootFinder<1, 1, (f64, f64)> for FalsePositionMethod {
         } else if fb.abs() < self.tol {
             return Ok([b]);
         } else if fa * fb > 0.0 {
-            bail!(RootError::NoRoot);
+            bail!(RootError::<1>::NoRoot);
         }
 
         for _ in 0..self.max_iter {
@@ -547,7 +552,7 @@ impl RootFinder<1, 1, (f64, f64)> for FalsePositionMethod {
                 a = c;
                 fa = fc;
             } else {
-                bail!(RootError::NoRoot);
+                bail!(RootError::<1>::NoRoot);
             }
         }
         let c = (a * fb - b * fa) / (fb - fa);
@@ -580,7 +585,7 @@ pub struct BroydenMethod {
 }
 
 
-#[allow(unused_variables)]
+#[allow(unused_variables, non_snake_case)]
 impl<const I: usize, const O: usize> RootFinder<I, O, Intv<I>> for BroydenMethod {
     fn max_iter(&self) -> usize {
         self.max_iter
@@ -592,6 +597,50 @@ impl<const I: usize, const O: usize> RootFinder<I, O, Intv<I>> for BroydenMethod
         &self,
         problem: &P,
     ) -> Result<Pt<I>> {
-        unimplemented!()
+        // Init state
+        let state = problem.initial_guess();
+        let (mut x0, mut x1) = state;
+        let mut fx0 = problem.function(x0)?.to_vec();
+
+        // Initialize inverse Jacobian via FDM
+        let h = 1e-6;
+        let mut J = zeros(O, I);
+        for o in 0..O {
+            for i in 0..I {
+                let mut x0h = x0;
+                x0h[i] += h;
+                let f1 = problem.function(x0h)?;
+                J[(o, i)] = (f1[o] - fx0[o]) / h;
+            }
+        }
+        let mut H = -J.pseudo_inv();
+
+        for _ in 0..self.max_iter {
+            let fx1 = problem.function(x1)?.to_vec();
+            if fx1.norm(Norm::L2) < self.tol {
+                return Ok(x1);
+            }
+            let dx = x1.iter().zip(x0.iter()).map(|(x1, x0)| x1 - x0).collect::<Vec<_>>();
+            if dx.norm(Norm::L2) < self.tol {
+                return Ok(x1);
+            }
+            let df = fx1.iter().zip(fx0.iter()).map(|(fx1, fx0)| fx1 - fx0).collect::<Vec<_>>();
+
+            let denom = dx.add_v(&H.apply(&df));
+            let right = &dx.to_row() * &H;
+            let num = right.apply(&df);
+
+            let left = denom.div_v(&num);
+
+            H = H + left.to_row() * right;
+
+            x0 = x1;
+            fx0 = fx1.clone();
+            let dx_new = H.apply(&fx1);
+            for i in 0..I {
+                x1[i] += dx_new[i];
+            }
+        }
+        bail!(RootError::NotConverge(x1));
     }
 }
